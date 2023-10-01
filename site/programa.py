5c96d282a88f59fd515591b5bfa19874b74e758c
ArthurEv <arthurev.sg034@academico.ifsul.edu.br>
2023-10-01 14:40:54 -0300

    arrumei umas coisas

--- site/programa.py
+++ site/programa.py
@@ -11,13 +11,16 @@ def paginacriacao():
 
 @app.route('/criar', methods=['POST'])
 def criar_post():
+    
+    try:
+        cor_pele = request.form.get('cor-pele')
+        formato_cabelo = request.form.get('formato-cabelo')
+        cor_cabelo = request.form.get('cor-cabelo')
+        imagem=cor_pele+"-"+formato_cabelo+"-"+cor_cabelo+".png"
 
-    cor_pele = request.form.get('cor-pele')
-    formato_cabelo = request.form.get('formato-cabelo')
-    cor_cabelo = request.form.get('cor-cabelo')
-    imagem=cor_pele+"-"+formato_cabelo+"-"+cor_cabelo+".png"
-
-    return render_template('resultado.html',imagem=imagem)
-
+        return render_template('resultado.html',imagem=imagem)
+    except:
+       return render_template('criacao.html',erro="Por favor selecione um de cada")
+        
 if __name__ == '__main__':
  app.run()
